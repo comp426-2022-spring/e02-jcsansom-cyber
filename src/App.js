eae445b387677081341ed269bb1433a1146e66d3
Jaycee Sansom <jcsansom@email.unc.edu>
2022-04-24 13:47:01 -0400

    e01 var

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
     let currentA04 = document.querySelector('#a04').value
     let currentA05 = document.querySelector('#a05').value
     let currentMidterm = document.querySelector('#midterm').value
-    
+    let currentE01 = document.querySelector('#e01').value
     if(currentSum==='')
     return;
 // 2. This line is where you add the points all together. Add your other variable references here. Make sure that they match what is above and what is below in the HTML form
